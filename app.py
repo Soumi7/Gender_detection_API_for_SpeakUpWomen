76307bb5c49af89b524817ab5aed5c28185e8ef0
soumi7 <soumibardhan10@gmail.com>
2021-04-15 20:27:21 +0530

    Added flask app

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 from keras.utils import get_file
 import numpy as np
-import os
 import cvlib as cv
 
 model_path = "./pretrained/gender_detection.model"
@@ -26,20 +25,36 @@ classes = ['man','woman']
 # Load the model
 @app.route('/')
 def home():
-    return render_template('index.html')
-
+    return "Gender Detection API working perfectly!"
 
 @app.route('/predict_api',methods=['POST','GET'])
 def predict():
     #for HTML GUI rendering
     file = request.files['file']
     print(file.filename)
-    if file.filename.split(".")[-1]!="png":
-        return render_template('Wrong_file_type.html')
+    file.save(file.filename)
+    image = cv2.imread(file.filename)
 
-    image = cv2.imread(args.image)
-
-    return resp
+    if image is None:
+        print("Could not read input image")     
+    face, confidence = cv.detect_face(image)
+    count = 0
+    for idx, f in enumerate(face):
+        (startX, startY) = f[0], f[1]
+        (endX, endY) = f[2], f[3]
+        cv2.rectangle(image, (startX,startY), (endX,endY), (0,255,0), 2)
+        face_crop = np.copy(image[startY:endY,startX:endX])
+        face_crop = cv2.resize(face_crop, (96,96))
+        face_crop = face_crop.astype("float") / 255.0
+        face_crop = img_to_array(face_crop)
+        face_crop = np.expand_dims(face_crop, axis=0)
+        conf = model.predict(face_crop)[0]
+        idx = np.argmax(conf)
+        label = classes[idx]
+        if count>1:
+            break
+        count+=1
+    return jsonify(label = label,count = count)
 
 
 if __name__ == '__main__':
